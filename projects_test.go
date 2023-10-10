b3fcd52a11c9cc89f60959c8ccfcb8b42cafa972
evgeniy.michurin <anaxaim@gmail.com>
2023-10-10 19:03:06 +0400

    refactor(CLOUDDEV-275): add get project endpoint

---
+++ projects_test.go
@@ -0,0 +1,34 @@
+package edgecloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestProjects_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	project := &Project{
+		ID:   1,
+		Name: "test-project",
+	}
+
+	mux.HandleFunc("/v1/projects/1", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		resp, _ := json.Marshal(project)
+		fmt.Fprintf(w, `{"project":%s}`, string(resp))
+	})
+
+	resp, _, err := client.Projects.Get(ctx, "1")
+	if err != nil {
+		t.Errorf("Projects.Get returned error: %v", err)
+	}
+
+	if !reflect.DeepEqual(resp, project) {
+		t.Errorf("Projects.Get returned %+v, expected %+v", resp, project)
+	}
+}
