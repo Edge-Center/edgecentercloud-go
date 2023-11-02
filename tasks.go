d3cde4fa5006ee8da1b4253f6d81494548cdc04a
Evgenyi Michurin <anaxaim@gmail.com>
2023-11-02 14:51:44 +0400

    refactor(CLOUDDEV-275): tasks

--- tasks.go
+++ tasks.go
@@ -10,10 +10,19 @@ const (
 	tasksBasePathV1 = "/v1/tasks"
 )
 
+const (
+	tasksActive      = "active"
+	tasksAcknowledge = "acknowledge"
+)
+
 // TasksService is an interface for managing Tasks with the EdgecenterCloud API.
 // See: https://apidocs.edgecenter.ru/cloud#tag/tasks
 type TasksService interface {
+	ListActive(context.Context) ([]Task, *Response, error)
+	Acknowledge(context.Context, string) (*Task, *Response, error)
+	AcknowledgeAll(context.Context, *TaskAcknowledgeAllOptions) (*Response, error)
 	Get(context.Context, string) (*Task, *Response, error)
+	List(context.Context, *TaskListOptions) ([]Task, *Response, error)
 }
 
 // TasksServiceOp handles communication with Tasks methods of the EdgecenterCloud API.
@@ -56,9 +65,104 @@ const (
 	TaskStateError    TaskState = "ERROR"
 )
 
+type TaskSorting string
+
+const (
+	TaskSortingAsc  TaskSorting = "asc"
+	TaskSortingDesc TaskSorting = "desc"
+)
+
 // TaskResponse is an EdgecenterCloud response with list of created tasks.
 type TaskResponse struct {
 	Tasks []string `json:"tasks"`
+}
+
+type TaskAcknowledgeAllOptions struct {
+	ProjectID int    `url:"project_id,omitempty" validate:"omitempty"`
+	RegionID  string `url:"region_id,omitempty" validate:"omitempty"`
+}
+
+type TaskListOptions struct {
+	ClientID          int         `url:"client_id,omitempty" validate:"omitempty"`
+	ProjectID         int         `url:"project_id,omitempty" validate:"omitempty"`
+	RegionID          int         `url:"region_id,omitempty" validate:"omitempty"`
+	ScheduleID        string      `url:"schedule_id,omitempty" validate:"omitempty"`
+	LifecyclePolicyID int         `url:"lifecycle_policy_id,omitempty" validate:"omitempty"`
+	IsAcknowledged    bool        `url:"is_acknowledged,omitempty" validate:"omitempty"`
+	FromTimestamp     string      `url:"from_timestamp,omitempty" validate:"omitempty"`
+	ToTimestamp       string      `url:"to_timestamp,omitempty" validate:"omitempty"`
+	Limit             int         `url:"limit,omitempty" validate:"omitempty"`
+	Offset            int         `url:"offset,omitempty" validate:"omitempty"`
+	TaskType          string      `url:"task_type,omitempty" validate:"omitempty"`
+	State             TaskState   `url:"state,omitempty" validate:"omitempty"`
+	Sorting           TaskSorting `url:"sorting,omitempty" validate:"omitempty"`
+}
+
+// tasksRoot represents a Tasks root.
+type tasksRoot struct {
+	Count int
+	Tasks []Task `json:"results"`
+}
+
+// ListActive get active tasks.
+func (s *TasksServiceOp) ListActive(ctx context.Context) ([]Task, *Response, error) {
+	if resp, err := s.client.Validate(); err != nil {
+		return nil, resp, err
+	}
+
+	path := fmt.Sprintf("%s/%s", s.client.addProjectRegionPath(tasksBasePathV1), tasksActive)
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	tasks := new(tasksRoot)
+	resp, err := s.client.Do(ctx, req, tasks)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return tasks.Tasks, resp, err
+}
+
+// Acknowledge one task on project scope.
+func (s *TasksServiceOp) Acknowledge(ctx context.Context, taskID string) (*Task, *Response, error) {
+	if resp, err := isValidUUID(taskID, "taskID"); err != nil {
+		return nil, resp, err
+	}
+
+	path := fmt.Sprintf("%s/%s/%s", tasksBasePathV1, taskID, tasksAcknowledge)
+
+	req, err := s.client.NewRequest(ctx, http.MethodPost, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	task := new(Task)
+	resp, err := s.client.Do(ctx, req, task)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return task, resp, err
+}
+
+// AcknowledgeAll client tasks in project or region.
+func (s *TasksServiceOp) AcknowledgeAll(ctx context.Context, opts *TaskAcknowledgeAllOptions) (*Response, error) {
+	path := fmt.Sprintf("%s/%s", tasksBasePathV1, tasksAcknowledge)
+
+	path, err := addOptions(path, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := s.client.NewRequest(ctx, http.MethodPost, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
 }
 
 // Get individual Task.
@@ -81,4 +185,25 @@ func (s *TasksServiceOp) Get(ctx context.Context, taskID string) (*Task, *Respon
 	}
 
 	return task, resp, err
+}
+
+// List gets tasks.
+func (s *TasksServiceOp) List(ctx context.Context, opts *TaskListOptions) ([]Task, *Response, error) {
+	path, err := addOptions(tasksBasePathV1, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	tasks := new(tasksRoot)
+	resp, err := s.client.Do(ctx, req, tasks)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return tasks.Tasks, resp, err
 }
--- tasks_test.go
+++ tasks_test.go
@@ -5,10 +5,70 @@ import (
 	"fmt"
 	"net/http"
 	"path"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestTasks_ListActive(t *testing.T) {
+	setup()
+	defer teardown()
+
+	expectedResp := []Task{{ID: taskID}}
+	URL := path.Join(tasksBasePathV1, strconv.Itoa(projectID), strconv.Itoa(regionID), tasksActive)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, _ = fmt.Fprintf(w, `{"results":%s}`, string(resp))
+	})
+
+	respActual, resp, err := client.Tasks.ListActive(ctx)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, respActual, expectedResp)
+}
+
+func TestTasks_Acknowledge(t *testing.T) {
+	setup()
+	defer teardown()
+
+	expectedResp := &Task{ID: taskID}
+	URL := path.Join(tasksBasePathV1, taskID, tasksAcknowledge)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, _ = fmt.Fprint(w, string(resp))
+	})
+
+	respActual, resp, err := client.Tasks.Acknowledge(ctx, taskID)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, respActual, expectedResp)
+}
+
+func TestTasks_AcknowledgeAll(t *testing.T) {
+	setup()
+	defer teardown()
+
+	URL := path.Join(tasksBasePathV1, tasksAcknowledge)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+	})
+
+	resp, err := client.Tasks.AcknowledgeAll(ctx, nil)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+}
 
 func TestTasks_Get(t *testing.T) {
 	setup()
@@ -27,6 +87,27 @@ func TestTasks_Get(t *testing.T) {
 	})
 
 	respActual, resp, err := client.Tasks.Get(ctx, taskID)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, respActual, expectedResp)
+}
+
+func TestTasks_List(t *testing.T) {
+	setup()
+	defer teardown()
+
+	expectedResp := []Task{{ID: taskID}}
+
+	mux.HandleFunc(tasksBasePathV1, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, _ = fmt.Fprintf(w, `{"results":%s}`, string(resp))
+	})
+
+	respActual, resp, err := client.Tasks.List(ctx, nil)
 	require.NoError(t, err)
 	require.Equal(t, resp.StatusCode, 200)
 	require.Equal(t, respActual, expectedResp)
