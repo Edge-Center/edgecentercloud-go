141c31471d5412254f56d32165e06c458789ce42
Evgenyi Michurin <anaxaim@gmail.com>
2023-11-02 15:23:19 +0400

    refactor(CLOUDDEV-275): quotas

--- quotas_test.go
+++ quotas_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"path"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -94,6 +96,69 @@ func TestQuotas_ListRegional(t *testing.T) {
 	})
 
 	respActual, resp, err := client.Quotas.ListRegional(ctx, options)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, respActual, expectedResp)
+}
+
+func TestQuotas_DeleteNotificationThreshold(t *testing.T) {
+	setup()
+	defer teardown()
+
+	clientID := 8
+	URL := path.Join(quotasClientBasePathV2, strconv.Itoa(clientID), quotasNotificationThreshold)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.Quotas.DeleteNotificationThreshold(ctx, clientID)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+}
+
+func TestNetworks_GetNotificationThreshold(t *testing.T) {
+	setup()
+	defer teardown()
+
+	clientID := 8
+	expectedResp := &QuotaNotificationThreshold{Threshold: 1}
+	URL := path.Join(quotasClientBasePathV2, strconv.Itoa(clientID), quotasNotificationThreshold)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, _ = fmt.Fprint(w, string(resp))
+	})
+
+	respActual, resp, err := client.Quotas.GetNotificationThreshold(ctx, clientID)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, respActual, expectedResp)
+}
+
+func TestNetworks_UpdateNotificationThreshold(t *testing.T) {
+	setup()
+	defer teardown()
+
+	clientID := 8
+	request := &NotificationThresholdUpdateRequest{Threshold: 1}
+	expectedResp := &QuotaNotificationThreshold{Threshold: 1}
+	URL := path.Join(quotasClientBasePathV2, strconv.Itoa(clientID), quotasNotificationThreshold)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPut)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, _ = fmt.Fprint(w, string(resp))
+	})
+
+	respActual, resp, err := client.Quotas.UpdateNotificationThreshold(ctx, clientID, request)
 	require.NoError(t, err)
 	require.Equal(t, resp.StatusCode, 200)
 	require.Equal(t, respActual, expectedResp)
