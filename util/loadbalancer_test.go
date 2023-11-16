2271dd0b04474bee7084a39d503f13a9b98b5166
Evgenyi Michurin <anaxaim@gmail.com>
2023-11-16 11:40:26 +0400

    feat(CLOUDDEV-337): loadbalancer tests

--- util/loadbalancer_test.go
+++ util/loadbalancer_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -258,6 +259,135 @@ func TestWaitLoadBalancerProvisioningStatusActive(t *testing.T) {
 	client.Project = projectID
 	client.Region = regionID
 
-	err := WaitLoadBalancerProvisioningStatusActive(context.Background(), client, testResourceID)
+	err := WaitLoadBalancerProvisioningStatusActive(context.Background(), client, testResourceID, nil)
 	assert.NoError(t, err)
+}
+
+func TestWaitLoadBalancerProvisioningStatusActive_Error(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	URL := path.Join("/v1/loadbalancers", strconv.Itoa(projectID), strconv.Itoa(regionID), testResourceID)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+
+	client := edgecloud.NewClient(nil)
+	baseURL, _ := url.Parse(server.URL)
+	client.BaseURL = baseURL
+	client.Project = projectID
+	client.Region = regionID
+
+	var attempts uint = 2
+	err := WaitLoadBalancerProvisioningStatusActive(context.Background(), client, testResourceID, &attempts)
+	assert.Error(t, err)
+}
+
+func TestWaitLoadBalancerProvisioningStatusActive_CustomErrors(t *testing.T) {
+	tests := []struct {
+		name               string
+		provisioningStatus edgecloud.ProvisioningStatus
+		expectErr          error
+	}{
+		{
+			name:               "ErrErrorState",
+			provisioningStatus: edgecloud.ProvisioningStatusError,
+			expectErr:          ErrErrorState,
+		},
+		{
+			name:               "ErrNotActiveStatus",
+			provisioningStatus: edgecloud.ProvisioningStatusPendingCreate,
+			expectErr:          ErrNotActiveStatus,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			mux := http.NewServeMux()
+			server := httptest.NewServer(mux)
+			defer server.Close()
+
+			expectedResp := edgecloud.Loadbalancer{
+				Name:               testResourceID,
+				ProvisioningStatus: tc.provisioningStatus,
+			}
+			URL := path.Join("/v1/loadbalancers", strconv.Itoa(projectID), strconv.Itoa(regionID), testResourceID)
+
+			mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+				resp, err := json.Marshal(expectedResp)
+				if err != nil {
+					t.Errorf("failed to marshal response: %v", err)
+				}
+				_, _ = fmt.Fprint(w, string(resp))
+			})
+
+			client := edgecloud.NewClient(nil)
+			baseURL, _ := url.Parse(server.URL)
+			client.BaseURL = baseURL
+			client.Project = projectID
+			client.Region = regionID
+
+			var attempts uint = 2
+			err := WaitLoadBalancerProvisioningStatusActive(context.Background(), client, testResourceID, &attempts)
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestFindPoolMemberByAddressPortAndSubnetID(t *testing.T) {
+	testAddress := net.IP("192.168.1.1")
+	testProtocolPort := 80
+	testPool := edgecloud.Pool{
+		Members: []edgecloud.PoolMember{
+			{
+				ID: testResourceID,
+				PoolMemberCreateRequest: edgecloud.PoolMemberCreateRequest{
+					Address:      testAddress,
+					ProtocolPort: testProtocolPort,
+					SubnetID:     testName,
+				},
+			},
+		},
+	}
+
+	found := FindPoolMemberByAddressPortAndSubnetID(testPool, testAddress, testProtocolPort, testName)
+	assert.True(t, found)
+}
+
+func TestFindPoolMemberByAddressPortAndSubnetID_MemberDoesNotExist(t *testing.T) {
+	testProtocolPort := 80
+	testPool := edgecloud.Pool{
+		Members: []edgecloud.PoolMember{
+			{
+				ID: testResourceID,
+				PoolMemberCreateRequest: edgecloud.PoolMemberCreateRequest{
+					Address:      net.IP("192.168.1.1"),
+					ProtocolPort: testProtocolPort,
+					SubnetID:     testName,
+				},
+			},
+		},
+	}
+
+	found := FindPoolMemberByAddressPortAndSubnetID(testPool, net.IP("192.168.1.2"), testProtocolPort, testName)
+	assert.False(t, found)
+}
+
+func TestFindPoolMemberByAddressPortAndSubnetID_MemberAddressIsNil(t *testing.T) {
+	testProtocolPort := 80
+	testPool := edgecloud.Pool{
+		Members: []edgecloud.PoolMember{
+			{
+				ID: testResourceID,
+				PoolMemberCreateRequest: edgecloud.PoolMemberCreateRequest{
+					ProtocolPort: testProtocolPort,
+					SubnetID:     testName,
+				},
+			},
+		},
+	}
+	found := FindPoolMemberByAddressPortAndSubnetID(testPool, net.IP("192.168.1.2"), testProtocolPort, testName)
+	assert.False(t, found)
 }
