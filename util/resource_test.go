e3bdc21001bdc7216401546da05f40ffa9c898b5
Evgenyi Michurin <anaxaim@gmail.com>
2023-11-20 13:06:03 +0400

    feat(CLOUDDEV-328): helper tests

--- util/resource_test.go
+++ util/resource_test.go
@@ -94,6 +94,12 @@ func TestResourceIsExist(t *testing.T) {
 			expectedError: nil,
 			exist:         false,
 		},
+		{
+			name:          "default case error",
+			statusCode:    http.StatusInternalServerError,
+			expectedError: fmt.Errorf("%w, status code: %d, details: %s", errGetResourceInfo, 500, "500"),
+			exist:         false,
+		},
 	}
 
 	for _, tt := range tests {
@@ -120,8 +126,161 @@ func TestResourceIsExist(t *testing.T) {
 			}
 
 			exist, err := ResourceIsExist(context.Background(), getFunc, testResourceID)
-			assert.Equal(t, tt.expectedError, err)
+			if tt.name == "default case error" {
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expectedError, err)
+			}
 			assert.Equal(t, tt.exist, exist)
 		})
 	}
+}
+
+func TestDeleteResourceIfExist(t *testing.T) {
+	client := edgecloud.NewClient(nil)
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		resource interface{}
+	}{
+		{
+			name:     "LoadbalancersService",
+			urlPath:  "/v1/loadbalancers",
+			resource: client.Loadbalancers,
+		},
+		{
+			name:     "FloatingIPsService",
+			urlPath:  "/v1/floatingips",
+			resource: client.Floatingips,
+		},
+		{
+			name:     "VolumesService",
+			urlPath:  "/v1/volumes",
+			resource: client.Volumes,
+		},
+		{
+			name:     "L7PoliciesService",
+			urlPath:  "/v1/l7policies",
+			resource: client.L7Policies,
+		},
+		{
+			name:     "SnapshotsService",
+			urlPath:  "/v1/snapshots",
+			resource: client.Snapshots,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mux := http.NewServeMux()
+			server := httptest.NewServer(mux)
+			defer server.Close()
+
+			baseURL, _ := url.Parse(server.URL)
+			client.BaseURL = baseURL
+			client.Project = projectID
+			client.Region = regionID
+
+			URL := path.Join(tt.urlPath, strconv.Itoa(projectID), strconv.Itoa(regionID), testResourceID)
+			mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+				switch r.Method {
+				case http.MethodDelete:
+					resp, _ := json.Marshal(&edgecloud.TaskResponse{Tasks: []string{testResourceID}})
+					_, _ = fmt.Fprint(w, string(resp))
+				case http.MethodGet:
+					w.WriteHeader(http.StatusNotFound)
+					resp, _ := json.MarshalIndent(edgecloud.Response{}, "", "    ")
+					_, _ = fmt.Fprint(w, string(resp))
+				}
+			})
+
+			mux.HandleFunc(path.Join("/v1/tasks", testResourceID), func(w http.ResponseWriter, r *http.Request) {
+				resp, err := json.Marshal(&edgecloud.Task{ID: testResourceID, State: edgecloud.TaskStateFinished})
+				if err != nil {
+					t.Fatalf("failed to marshal JSON: %v", err)
+				}
+				_, _ = fmt.Fprint(w, string(resp))
+			})
+
+			err := DeleteResourceIfExist(context.Background(), client, tt.resource, testResourceID)
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestDeleteResourceIfExist_deleteAndWait_Error(t *testing.T) {
+	client := edgecloud.NewClient(nil)
+
+	tests := []struct {
+		name     string
+		urlPath  string
+		resource interface{}
+	}{
+		{
+			name:     "LoadbalancersService",
+			urlPath:  "/v1/loadbalancers",
+			resource: client.Loadbalancers,
+		},
+		{
+			name:     "FloatingIPsService",
+			urlPath:  "/v1/floatingips",
+			resource: client.Floatingips,
+		},
+		{
+			name:     "VolumesService",
+			urlPath:  "/v1/volumes",
+			resource: client.Volumes,
+		},
+		{
+			name:     "L7PoliciesService",
+			urlPath:  "/v1/l7policies",
+			resource: client.L7Policies,
+		},
+		{
+			name:     "SnapshotsService",
+			urlPath:  "/v1/snapshots",
+			resource: client.Snapshots,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mux := http.NewServeMux()
+			server := httptest.NewServer(mux)
+			defer server.Close()
+
+			baseURL, _ := url.Parse(server.URL)
+			client.BaseURL = baseURL
+			client.Project = projectID
+			client.Region = regionID
+
+			URL := path.Join(tt.urlPath, strconv.Itoa(projectID), strconv.Itoa(regionID), testResourceID)
+			mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusInternalServerError)
+			})
+
+			mux.HandleFunc(path.Join("/v1/tasks", testResourceID), func(w http.ResponseWriter, r *http.Request) {
+				resp, err := json.Marshal(&edgecloud.Task{ID: testResourceID, State: edgecloud.TaskStateFinished})
+				if err != nil {
+					t.Fatalf("failed to marshal JSON: %v", err)
+				}
+				_, _ = fmt.Fprint(w, string(resp))
+			})
+
+			err := DeleteResourceIfExist(context.Background(), client, tt.resource, testResourceID)
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestDeleteResourceIfExist_ResourceNotSupported(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	defer server.Close()
+
+	client := edgecloud.NewClient(nil)
+
+	err := DeleteResourceIfExist(context.Background(), nil, client.Flavors, testResourceID)
+	assert.Equal(t, err, errDeleteResourceIfExistIsNotSupported)
 }
