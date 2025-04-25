43bbec57e878deb2645cc406a2697d918cb79d15
Aleksandr Semenov <aleksandr.semenov@edgecenter.ru>
2025-04-25 15:56:42 +0300

    feat(CLOUDDEV-1456): Add reseller images v2

---
+++ resellerimagesV2.go
@@ -0,0 +1,115 @@
+package edgecloud
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+const (
+	resellerImageBasePathV2 = "/v2/reseller_image"
+)
+
+type (
+	EntityID   = int
+	EntityType = string
+)
+
+const (
+	ResellerType EntityType = "reseller"
+	ClientType   EntityType = "client"
+	ProjectType  EntityType = "project"
+)
+
+// ResellerImageV2Service is an interface for managing Reseller images with the EdgecenterCloud API.
+// See: https://apidocs.edgecenter.ru/cloud_resellers#tag/Images
+type ResellerImageV2Service interface {
+	List(context.Context, EntityType, EntityID) (*ResellerImageV2List, *Response, error)
+	Delete(context.Context, EntityType, EntityID, *ResellerImageV2DeleteOptions) (*Response, error)
+	Update(context.Context, *ResellerImageV2UpdateRequest) (*ResellerImageV2, *Response, error)
+}
+
+// ResellerImageV2ServiceOp handles communication with Reseller images methods of the EdgecenterCloud API.
+type ResellerImageV2ServiceOp struct {
+	client *Client
+}
+
+var _ ResellerImageV2Service = &ResellerImageV2ServiceOp{}
+
+// ResellerImageV2List represents an EdgecenterCloud reseller images list.
+type ResellerImageV2List struct {
+	Count   int               `json:"count"`
+	Results []ResellerImageV2 `json:"results"`
+}
+
+// ResellerImageV2 represents an EdgecenterCloud reseller images.
+type ResellerImageV2 struct {
+	ImageIDs   *ImageIDs  `json:"image_ids"`
+	RegionID   int        `json:"region_id"`
+	EntityID   EntityID   `json:"entity_id"`
+	EntityType EntityType `json:"entity_type"`
+	CreatedAt  string     `json:"created_at,omitempty"`
+	UpdatedAt  string     `json:"updated_at,omitempty"`
+}
+
+// ResellerImageV2UpdateRequest represents a request to update available image list for a reseller, client, project.
+type ResellerImageV2UpdateRequest ResellerImageV2
+
+type ResellerImageV2DeleteOptions struct {
+	RegionID int `url:"region_id"`
+}
+
+// List get available image IDs limits for a reseller, client, project.
+func (s *ResellerImageV2ServiceOp) List(ctx context.Context, entityType EntityType, entityID EntityID) (*ResellerImageV2List, *Response, error) {
+	pathReq := fmt.Sprintf("%s/%s/%d", resellerImageBasePathV2, entityType, entityID)
+
+	req, err := s.client.NewRequest(ctx, http.MethodGet, pathReq, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ril := new(ResellerImageV2List)
+	resp, err := s.client.Do(ctx, req, ril)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return ril, resp, nil
+}
+
+// Update set or update available image list for a reseller, client, project.
+func (s *ResellerImageV2ServiceOp) Update(ctx context.Context, reqBody *ResellerImageV2UpdateRequest) (*ResellerImageV2, *Response, error) {
+	if reqBody == nil {
+		return nil, nil, NewArgError("reqBody", "cannot be nil")
+	}
+
+	req, err := s.client.NewRequest(ctx, http.MethodPut, resellerImageBasePathV2, reqBody)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	rImage := new(ResellerImageV2)
+	resp, err := s.client.Do(ctx, req, rImage)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return rImage, resp, nil
+}
+
+// Delete image limits for reseller, client, project.
+func (s *ResellerImageV2ServiceOp) Delete(ctx context.Context, entityType EntityType, entityID EntityID, opts *ResellerImageV2DeleteOptions) (*Response, error) {
+	pathReq := fmt.Sprintf("%s/%s/%d", resellerImageBasePathV2, entityType, entityID)
+
+	pathReq, err := addOptions(pathReq, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := s.client.NewRequest(ctx, http.MethodDelete, pathReq, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
---
+++ resellerimagesV2_test.go
@@ -0,0 +1,109 @@
+package edgecloud
+
+import (
+	"encoding/json"
+	"net/http"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestResellerImagesV2_List(t *testing.T) {
+	setup()
+	defer teardown()
+
+	ids := ImageIDs{
+		"b5b4d65d-945f-4b98-ab6f-332319c724ef",
+		"0052a312-e6d8-4177-8e29-b017a3a6b588",
+	}
+
+	ri := ResellerImageV2{
+		ImageIDs:   &ids,
+		RegionID:   8,
+		EntityType: ResellerType,
+		EntityID:   123456,
+	}
+
+	expectedResp := ResellerImageV2List{
+		Count:   1,
+		Results: []ResellerImageV2{ri},
+	}
+
+	URL := path.Join(resellerImageBasePathV2, "/", "reseller", "/", "123456")
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, err = w.Write(resp)
+		if err != nil {
+			t.Errorf("failed to write response: %v", err)
+		}
+	})
+
+	respActual, resp, err := client.ResellerImageV2.List(ctx, "reseller", 123456)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, *respActual, expectedResp)
+}
+
+func TestResellerImagesV2_Update(t *testing.T) {
+	setup()
+	defer teardown()
+
+	ids := ImageIDs{
+		"b5b4d65d-945f-4b98-ab6f-332319c724ef",
+		"0052a312-e6d8-4177-8e29-b017a3a6b588",
+	}
+
+	updateReq := ResellerImageV2UpdateRequest{
+		ImageIDs:   &ids,
+		RegionID:   8,
+		EntityType: ResellerType,
+		EntityID:   123456,
+	}
+
+	expectedResp := ResellerImageV2{
+		ImageIDs:   &ids,
+		RegionID:   8,
+		EntityType: ResellerType,
+		EntityID:   123456,
+	}
+
+	URL := path.Join(resellerImageBasePathV2)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPut)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, err = w.Write(resp)
+		if err != nil {
+			t.Errorf("failed to write response: %v", err)
+		}
+	})
+
+	respActual, resp, err := client.ResellerImageV2.Update(ctx, &updateReq)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, *respActual, expectedResp)
+}
+
+func TestResellerImagesV2_Delete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	URL := path.Join(resellerImageBasePathV2, "/", "reseller", "/", "123456")
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodDelete)
+	})
+
+	resp, err := client.ResellerImageV2.Delete(ctx, ResellerType, 123456, nil)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+}
