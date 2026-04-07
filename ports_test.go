fc415501a5ead9c96ab0826f6a4fc9ff01d8b170
Leonid Stepanov <leonid.stepanov@edgecener.ru>
2026-04-07 20:48:48 +0300

    feat(CLOUDMKAAS-60): get allow addres pairs

--- ports_test.go
+++ ports_test.go
@@ -83,3 +83,25 @@ func TestPorts_DisablePortSecurity(t *testing.T) {
 	require.Equal(t, resp.StatusCode, 200)
 	require.Equal(t, respActual, expectedResp)
 }
+
+func TestPorts_GetAllowAddressPairs(t *testing.T) {
+	setup()
+	defer teardown()
+
+	expectedResp := &Port{PortID: testResourceID, AllowedAddressPairs: []PortsAllowedAddressPairs{{IPAddress: "192.168.0.2"}}}
+	URL := path.Join(portsBasePathV1, strconv.Itoa(projectID), strconv.Itoa(regionID), testResourceID, portsAllowAddressPairs)
+
+	mux.HandleFunc(URL, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodGet)
+		resp, err := json.Marshal(expectedResp)
+		if err != nil {
+			t.Errorf("failed to marshal response: %v", err)
+		}
+		_, _ = fmt.Fprint(w, string(resp))
+	})
+
+	respActual, resp, err := client.Ports.GetAllowAddressPairs(ctx, testResourceID)
+	require.NoError(t, err)
+	require.Equal(t, resp.StatusCode, 200)
+	require.Equal(t, respActual, expectedResp)
+}
